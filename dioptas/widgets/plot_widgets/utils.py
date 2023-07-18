43c29a755d69606749aa0cb29b1f5755713d3b2b
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-07-18 14:07:10 +0200

    rework auto_level function to use image data instead of histogram

--- dioptas/widgets/plot_widgets/utils.py
+++ dioptas/widgets/plot_widgets/utils.py
@@ -19,75 +19,75 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import annotations
 
+from typing import Optional
 import numpy as np
 
 
-def weighted_average_std(a: np.ndarray, weights: np.ndarray) -> tuple[float, float]:
-    """Returns the weighted average and weighted standard deviation"""
-    mean, sum_of_weights = np.average(a, weights=weights, returned=True)
-    variance = np.sum(weights * (a - mean)  ** 2) / sum_of_weights
-    return mean, np.sqrt(variance)
+def _default_auto_level(data: np.ndarray) -> tuple[float, float]:
+    """Compute colormap range from data through histogram
 
-
-def _default_auto_level(hist_x: np.ndarray, hist_y: np.ndarray) -> tuple[float, float]:
-    """Compute colormap range from histogram of the data
-
-    :param hist_x: Bin left edges
-    :param hist_y: Histogram count
+    :param data:
     :returns: (min, max)
     """
-    hist_y_cumsum = np.cumsum(hist_y)
-    hist_y_sum = np.sum(hist_y)
+    counts, bin_edges = np.histogram(data, bins=3000)
+    left_edges = bin_edges[:-1]
+    left_edges = left_edges[np.where(counts>0)]
+    counts = counts[np.where(counts>0)]
+
+    hist_y_cumsum = np.cumsum(counts)
+    hist_y_sum = np.sum(counts)
 
     max_ind = np.where(hist_y_cumsum < (0.996 * hist_y_sum))
-    min_level = np.mean(hist_x[:2])
+    min_level = np.mean(left_edges[:2])
 
     if len(max_ind[0]):
-        max_level = hist_x[max_ind[0][-1]]
+        max_level = left_edges[max_ind[0][-1]]
     else:
-        max_level = 0.5 * np.max(hist_x)
+        max_level = 0.5 * np.max(left_edges)
 
-    if len(hist_x[hist_x > 0]) > 0:
-        min_level = max(min_level, np.nanmin(hist_x[hist_x > 0]))
+    if len(left_edges[left_edges > 0]) > 0:
+        min_level = max(min_level, np.nanmin(left_edges[left_edges > 0]))
     return min_level, max_level
 
 
-def _minmax_auto_level(hist_x: np.ndarray, hist_y: np.ndarray) -> tuple[float, float]:
+def _minmax_auto_level(data: np.ndarray) -> tuple[float, float]:
     """Returns min/max of the data
 
-    :param hist_x: Bin left edges
-    :param hist_y: Histogram count
     :returns: (min, max)
     """
-    bin_size = hist_x[-1] - hist_x[-2]
-    return hist_x[0], hist_x[-1] + bin_size
+    return float(np.min(data)), float(np.max(data))
 
 
-def _mean3std_auto_level(hist_x: np.ndarray, hist_y: np.ndarray) -> tuple[float, float]:
+def _mean3std_auto_level(data: np.ndarray) -> tuple[float, float]:
     """Returns mean+/-3std clipped to min/max of the data
 
-    :param hist_x: Bin left edges
-    :param hist_y: Histogram count
     :returns: (lower limit, upper limit)
     """
-    mean, std = weighted_average_std(hist_x, hist_y)
-    minimum, maximum = _minmax_auto_level(hist_x, hist_y)
+    mean = np.mean(data, dtype=np.float64)
+    std = np.std(data, dtype=np.float64)
+    minimum, maximum = _minmax_auto_level(data)
     return max(mean - 3 * std, minimum), min(mean + 3 * std, maximum)
 
 
-def auto_level(hist_x: np.ndarray, hist_y: np.ndarray, mode: str = 'default') -> tuple[float, float]:
-    """Compute colormap range from histogram of the data
+def auto_level(data: Optional[np.ndarray], mode: str = 'default') -> Optional[tuple[float, float]]:
+    """Compute colormap range from data
 
-    :param hist_x: Bin left edges
-    :param hist_y: Histogram count
+    :param data: Data from which to compute colormap range
     :param mode: Mode of autoscale computation: "default", "minmax", "mean3std"
-    :returns: (min, max)
+    :returns: (min, max) or None
     :raise ValueError: If the mode is not supported
     """
+    if data is None:
+        return None
+
+    filtered_data = data[np.isfinite(data)]
+    if filtered_data.size == 0:
+        return None
+
     if mode == 'default':
-        return _default_auto_level(hist_x, hist_y)
+        return _default_auto_level(filtered_data)
     if mode == 'minmax':
-        return _minmax_auto_level(hist_x, hist_y)
+        return _minmax_auto_level(filtered_data)
     if mode == 'mean3std':
-        return _mean3std_auto_level(hist_x, hist_y)
+        return _mean3std_auto_level(filtered_data)
     raise ValueError(f'Unsupported mode: {mode}')
